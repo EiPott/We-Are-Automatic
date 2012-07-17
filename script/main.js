8288105f5effb0f6619f9c6bc3879f290f6b6d43
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-07-17 21:20:18 +0200

    moved javascript to external file

---
+++ script/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("#e-mail").click(function(){
+        window.open(
+            'http://mail.google.com/mail/u/0/?view=cm&amp;fs=1&amp;tf=1&amp;to=kontakt@weareautomatic.com',
+		    'Compose new message',
+		    'width=620px,height=480px'
+		);
+		return false;
+    });
+    
+});
